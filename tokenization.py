34f7a7c016826e26c527b2470d3d32ea6fec1349
Britton Winterrose <BrittonWinterrose@users.noreply.github.com>
2019-05-05 01:05:32 -0700

    Update tokenization.py

--- tokenization.py
+++ tokenization.py
@@ -85,7 +85,7 @@ def convert_to_unicode(text):
     elif isinstance(text, bytes):
       return text.decode("utf-8", "ignore")
     else:
-      raise ValueError("Unsupported string type: %s" % (type(text)))
+      raise ValueError("Unsupported string type: %s" % (type(text)), "Text:", text)
   elif six.PY2:
     if isinstance(text, str):
       return text.decode("utf-8", "ignore")
